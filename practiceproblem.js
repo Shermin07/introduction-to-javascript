4535301667bb6fad6084e7e5c768d6028ae81a05
Shermin Sultana <rima64549@gmail.com>
2023-06-26 00:56:34 +0600

    first commit

---
+++ practiceproblem.js
@@ -0,0 +1,37 @@
+  //  practice 1
+var oranges = 400;
+var apples = 300;
+var totalCost = oranges+apples;
+console.log(totalCost);
+var totalMoney = 1000;
+var totalCost = 700;
+var remain = totalMoney-totalCost;
+console.log(remain);
+
+//   practice 2
+var Mathmetics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var totalMarks = Mathmetics+Biology+Chemistry+Physics+Bangla;
+console.log(totalMarks);
+var totalMarks = 355.2;
+var subjectNumber = 5;
+var average = totalMarks / subjectNumber;
+average = average. toFixed(2);
+console.log(average);
+
+//   practice 3
+
+var firstLine = "I am going to be"
+var secondLine = "an awesome web developer"
+var fullLine = firstLine+" "+secondLine;
+console.log(fullLine);
+
+//  practice 4
+var givenNumber = 119;
+var divider = 5;
+var remainder = 119%5;
+console.log(remainder);
+
---
+++ variable.js
@@ -0,0 +1,18 @@
+//  very+able=veriable
+ // var name = value;
+//  numeric
+var price = 100;
+var age = 30;
+var money = 1000;
+var temperature = 28;
+var weight = 60;
+var height = 160;
+
+// string
+var person = "shermin sultana";
+var location ="finland";
+var special = "gm";
+
+//boolean
+var ishappy = true;
+var issad = false;
