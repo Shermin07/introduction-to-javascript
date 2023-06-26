bd306f934978c99d870a6b0f3d5d044c46a7bd1b
Shermin Sultana <rima64549@gmail.com>
2023-06-27 04:19:33 +0600

    conditions

---
+++ multi.stage.condition.js
@@ -0,0 +1,29 @@
+var money = 25;
+var danishPrice = 20;
+var butterBread = 30;
+var chocolateBiscuit = 50;
+if(money>danishPrice){
+    console.log("i wii take danish");
+}
+else if(money==butterBread){
+    console.log("i wii take bread"); 
+}
+else if(money<=chocolateBiscuit){
+    console.log("i wii take biscuit"); 
+}
+else{
+    console.log("i wii take coffee")
+}
+
+
+var math = true;
+var geometry = true;
+var straightLine = true;
+if(math!=geometry){ if(geometry==true){
+    if(straightLine==true)
+    console.log("alimpead");}
+
+}
+else{
+    console.log("pithagoreous");
+}
---
+++ multiple.conditions.js
@@ -0,0 +1,19 @@
+var isGraduated = true;
+var salary = 50000;
+var cars = 0
+if(isGraduated==true && salary<=55000 && cars<=1) {
+    console.log("i wii try to get a job");
+}
+else{ console.log("i wii try to get merried");
+
+}
+var isGraduated = false;
+var salary = 5000;
+var cars = 1
+if(isGraduated==true || salary>=55000 || cars<=2) {
+    console.log("i wii try to get a job");
+}
+else{ console.log("i wii try to get merried");
+
+}
+
