5d70ce9ce7c879f843329414a4b992c334ed85aa
Shermin Sultana <rima64549@gmail.com>
2023-06-30 02:50:14 +0600

    array

---
+++ array-loop.js
@@ -0,0 +1,16 @@
+for(var i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+var numbers = [45, 87, 89, 56, 32, 51, 25];
+for(var i = 0; i < 7; i+=1){
+var number = numbers[i];
+    console.log(number);
+}
+
+
+var items = ["pen", "book", "bottle", "laptop", "notebook", "pencil"];
+for (var i = 0; i<items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
--- for.js
+++ for.js
@@ -1,5 +1,12 @@
-var roastGiven = 0;
+ //var roastGiven = 0;
 while (roastGiven < 7 ){
     console.log("taking roast");
     roastGiven+=1;
+}
+ for(var roastGiven = 0; roastGiven < 7; roastGiven+=1){
+    console.log("i will take one");
+}
+
+for(var i=1; i < 7; i+=2){
+    console.log("mine");
 }
